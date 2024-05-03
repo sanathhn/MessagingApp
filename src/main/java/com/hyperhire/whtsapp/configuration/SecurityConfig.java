91922dea76ff3004836cbcdd9a5841bf687f809e
sanathhn <sanathnagraj5@gmail.com>
2024-05-03 16:18:04 +0530

    added swagger and security

--- src/main/java/com/hyperhire/whtsapp/configuration/SecurityConfig.java
+++ src/main/java/com/hyperhire/whtsapp/configuration/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.hyperhire.whtsapp.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,23 +17,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/public/**").permitAll() 
-                .anyRequest().authenticated() 
-            .and()
-            .formLogin() 
-                .loginPage("/login") 
-                .permitAll() 
-            .and()
-            .logout()
-                .permitAll();
+                .anyRequest().authenticated()
+                .and()
+            .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER") 
-                .and()
-                .withUser("admin").password("{noop}admin").roles("USER", "ADMIN"); 
+                .withUser("user") 
+                .password("password") 
+                .roles("USER"); 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); 
     }
 }
